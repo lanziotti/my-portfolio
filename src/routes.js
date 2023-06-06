96ccf4ff4b1efc65107a1dbf22b30c01e8183415
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2023-06-06 11:00:45 -0300

    Corrigindo scroll top

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import CubosFlix from './pages/Projects/CubosFlix';
 import CubosPuzzle from './pages/Projects/CubosPuzzle';
 import DigitalBanking from './pages/Projects/DigitalBanking';
 import Dindin from './pages/Projects/Dindin';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
 
 function AppRoutes() {
     return (
         <>
+            <ScrollToTop />
             <ToastContainer />
             <Routes>
                 <Route path='/' element={<Home />} />
